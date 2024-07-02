e2b6c89aaf3e288178409b34e93455f86b366f15
Stepan Halych <stephengalych@gmail.com>
2024-07-03 01:12:39 +0300

    Added Entrypoint to docker execution

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,18 @@ RUN apt-get update && \
     git \
     unzip
 
+RUN curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && \
+    chmod +x wp-cli.phar && \
+    mv wp-cli.phar /usr/local/bin/wp
+
 EXPOSE 80
 
+COPY entrypoint.sh /usr/local/bin/entrypoint.sh
+
+RUN chmod +x /usr/local/bin/entrypoint.sh
+
+# Set the entrypoint to the custom script
+ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
+
+# Use the default command for the container
 CMD ["apache2-foreground"]
---
+++ entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+WP_PATH="/var/www/html"
+
+echo "===== Checking if WordPress is installed... ====="
+if [ ! -f "$WP_PATH/wp-config.php" ]; then
+    echo "===== Downloading WordPress... ====="
+    wp core download --path=$WP_PATH --allow-root
+
+    echo "===== Creating wp-config.php... ====="
+    wp config create --dbname=exampledb --dbuser=exampleuser --dbpass=examplepass --dbhost=db --path=$WP_PATH --allow-root
+    sleep 8
+
+    echo "Defining WP_HOME and WP_SITEURL in wp-config.php..."
+    echo "define('WP_HOME', 'http://localhost:8000');" >> $WP_PATH/wp-config.php
+    echo "define('WP_SITEURL', 'http://localhost:8000');" >> $WP_PATH/wp-config.php
+fi
+
+if ! wp core is-installed --allow-root --path=$WP_PATH; then
+    wp core install --url='http://localhost:8000' --title='Crypto Blocks Demo' --admin_user='admin' --admin_password='admin' --admin_email='admin@example.com' --allow-root --path=$WP_PATH
+    echo "===== WordPress installation completed. ====="
+fi
+
+
+wp plugin activate crypto-blocks --allow-root --path=$WP_PATH
+echo "===== Plugin 'crypto-blocks' activated. ====="
+
+
+
+echo "===== Starting Apache... ====="
+apache2-foreground
+
+# Keep the script running to prevent container exit
+tail -f /dev/null
